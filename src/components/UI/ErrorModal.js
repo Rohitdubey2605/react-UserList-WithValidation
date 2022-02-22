78a66e5e12910cdd574fb00263c7945b5853e7a2
Rohit Dubey <Rohit.Dubey@odessainc.com>
2022-02-22 23:48:55 +0530

    Portals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,28 +1,51 @@
-import Wrapper from "../Helpers/Wrapper";
+import { Fragment } from "react";
+// import Wrapper from "../Helpers/Wrapper";
 import Button from "./Button";
 import Card from "./Card";
 import styles from "./ErrorModal.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.cancel}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button type="cancel" onClick={props.cancel}>
+          Close
+        </Button>
+      </footer>
+    </Card>
+  );
+};
 
 const ErrorModal = (props) => {
-    const OnCancel = ()=>
-    {
-        props.cancel();
-    }
+  // const OnCancel = () => {
+  //   props.cancel();
+  // };
   return (
-    <Wrapper>
-      <div className={styles.backdrop} onClick={OnCancel} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button type="cancel" onClick={OnCancel}>Close</Button>
-        </footer>
-      </Card>
-    </Wrapper>
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop cancel={props.cancel} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          cancel={props.cancel}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </Fragment>
   );
 };
 
